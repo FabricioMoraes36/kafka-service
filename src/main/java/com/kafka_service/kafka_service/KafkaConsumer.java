c6580d032bdb0b8fc6db2eb229608fc6e0d41130
Fabricio Moraes <fabricio.benedito@agi.com.br>
2026-02-23 10:08:33 -0300

    Fix build e add kafka consumer

--- src/main/java/com/kafka_service/kafka_service/KafkaConsumer.java
+++ src/main/java/com/kafka_service/kafka_service/KafkaConsumer.java
@@ -1,7 +1,19 @@
 package com.kafka_service.kafka_service;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+
+import java.util.function.Consumer;
 
 @Configuration
 public class KafkaConsumer {
+
+    @Bean
+    public Consumer<Message<String>> testConsumer(){
+        return message -> {
+            System.out.println("consumindo");
+            System.out.println(message.getPayload());
+        };
+    }
 }
